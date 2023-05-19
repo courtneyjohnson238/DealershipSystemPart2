a0618b5f6e43a09243179cabd63695a27139e91e
courtneyjohnson238 <courtneyjohnson238@gmail.com>
2023-05-19 15:37:56 -0400

    finished phase 2

--- src/main/java/com/yearup/dealership/ContractDataManager.java
+++ src/main/java/com/yearup/dealership/ContractDataManager.java
@@ -6,18 +6,28 @@ import java.io.IOException;
 
 public class ContractDataManager {
 
-   public void saveContract (Contract contract){
-      /* try (BufferedWriter bw = new BufferedWriter(new FileWriter("contracts.csv"))) {
-           if ()
-           bw.write("")
-           bw.newLine();
+   public void saveContract (Contract contract) {
+      try (BufferedWriter bw = new BufferedWriter(new FileWriter("contracts.csv",true))) {
+         if (contract instanceof SalesContract){
+            bw.write("SALES" + "|" + contract.getDate() + "|" + contract.getCustomerName() + "|" + contract.getCustomerEmail() + "|" +
+                    contract.getVehicleSold().getVin() + "|" + contract.getVehicleSold().getYear() + "|" + contract.getVehicleSold().getMake()
+             + "|" + contract.getVehicleSold().getModel() + "|" + contract.getVehicleSold().getVehicleType() + "|" + contract.getVehicleSold()
+                    .getColor() + "|" + contract.getVehicleSold().getOdometer() + "|" + contract.getVehicleSold().getPrice() + "|" + ((SalesContract) contract).getSalesTaxAmount() +
+                    "|" + ((SalesContract) contract).getRecordingFee() + "|" + ((SalesContract) contract).getProcessingFee() + "|" + contract.getTotalPrice() + "|" + ((SalesContract) contract).isFinancing()
+            + "|" + contract.getMonthlyPayment());
+         } else if (contract instanceof LeaseContract) {
+            bw.write("LEASE" + "|" + contract.getDate() + "|" + contract.getCustomerName() + "|" + contract.getCustomerEmail() + "|" +
+                    contract.getVehicleSold().getVin() + "|" + contract.getVehicleSold().getYear() + "|" + contract.getVehicleSold().getMake()
+                    + "|" + contract.getVehicleSold().getModel() + "|" + contract.getVehicleSold().getVehicleType() + "|" + contract.getVehicleSold()
+                    .getColor() + "|" + contract.getVehicleSold().getOdometer() + "|" + contract.getVehicleSold().getPrice() + "|" + ((LeaseContract) contract).getExpectedEndingValue() + "|"
+                     + ((LeaseContract) contract).getLeaseFee() + "|" + contract.getTotalPrice() + "|" + contract.getMonthlyPayment());
+         }
+         bw.newLine();
 
 
-
-       } catch (IOException e) {
-           e.printStackTrace();
-
+      } catch (IOException e) {
+         e.printStackTrace();
 
 
-       }*/
-}
+      }
+   }}
